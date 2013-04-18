cf08334144af31c382e2d917774a602ad637c14e
Aaron Murray <aaron@aaron-murray.net>
2013-04-17 17:40:45 -0700

    Updating data check script

--- data_import.js
+++ data_import.js
@@ -21,9 +21,40 @@ tmp_cigars[2] = require('./tmp_data/cigars2.js');
 tmp_cigars[3] = require('./tmp_data/cigars3.js');
 tmp_cigars[4] = require('./tmp_data/cigars4.js');
 
+function clean_domain_values(raw_data, valid_values) {
+  if(!('contains' in String.prototype))
+    String.prototype.contains = function(str, startIndex) { return -1!==this.indexOf(str, startIndex); };
+  var cleaned_values = valid_values.filter(function (value) {
+    if (value == 'Connecticut') {
+      return raw_data[0].contains(value) && (!raw_data[0].contains('Connecticut Broadleaf') && !raw_data[0].contains('Connecticut Shade'));
+    } else if (value == 'Criollo') {
+      return raw_data[0].contains(value) && (!raw_data[0].contains('Havana 98 Criollo'));
+    } else {
+      return raw_data[0].contains(value);
+    }
+  });
+  return cleaned_values;
+}
+
 console.log('CHECKING DATA');
-for (var i=0;i<=4;i++) {
-  console.log('===============================');
-  console.log('Data Set '+ i + ': '+ tmp_cigars[i].length);
-  console.log('===============================');
+for (var i=1;i<=4;i++) {
+  for (var j=0;tmp_cigars[i][j];j++) {
+    var curr_cigar = tmp_cigars[i][j];
+    if (curr_cigar['binder']) {
+      curr_cigar.binder = curr_cigar.binder.split(',');
+      curr_cigar.binder = clean_domain_values(curr_cigar.binder, tmp_domains.binders);
+    }
+    if (curr_cigar['filler']) {
+      curr_cigar.filler = curr_cigar.filler.split(',');
+      curr_cigar.filler = clean_domain_values(curr_cigar.filler, tmp_domains.binders);
+    }
+    if (curr_cigar['wrapper']) {
+      curr_cigar.wrapper = curr_cigar.wrapper.split(',');
+      curr_cigar.wrapper = clean_domain_values(curr_cigar.wrapper, tmp_domains.wrappers);
+    }
+    var new_cigar = new Cigar();
+    new_cigar.name = curr_cigar.cigar_name;
+    new_cigar.brand = curr_cigar.brand;
+
+  }
 }
