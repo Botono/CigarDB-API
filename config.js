b96717cc2b3c4aff60342b5ee6e29097545bc7aa
Aaron <aaron@aaron-murray.net>
2013-05-02 20:39:00 -0700

    Updating mongodb credentials

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.creds = {
     // Your mongo auth uri goes here
     // e.g. mongodb://username:server@mongoserver:10059/somecollection
-    mongoose_auth: 'mongodb://user_cigardb:1iKgcsVPmhgUf7fkXX5Ixpi1x#MUZU@dharma.mongohq.com:10076/cigardb'
+    mongoose_auth: 'mongodb://user_cigardb:o6NHLjiLWx8KJ0Rvz8xFgAFV0N9pTG@dharma.mongohq.com:10076/cigardb'
 };
